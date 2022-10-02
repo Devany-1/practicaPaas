ad724c5adb78b6d02d186ae7be56942a75aa671e
Devany-1 <114431958+Devany-1@users.noreply.github.com>
2022-10-01 20:48:43 -0500

    Add files via upload

---
+++ CV-Devany/index.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CV</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+	rel="stylesheet">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+	<script src="https://kit.fontawesome.com/be0be6fea0.js" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+
+	<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Sobre mí</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="navbar-nav mr-auto" id="navbarNav">
+				<ul class="navbar-nav" >
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#estudios">Estudios</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#portafolio">Portafolio</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#intereses">Intereses</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#contacto">Contacto</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<br><br>
+	<br><br>
+
+	<div class="container" >
+		<div class="row ">
+			<div class="col text-center">
+				<h2>Devany Carmen Venegas Garcia</h2>	<br><br>
+				<p>Me considero una persona muy amable que se esfuerza cada día por ser una persona mejor conmigo misma mejorando día a día, una perosna con grandes sueños y metas por cumplir porque para mi las cosas siempre parecen imposibles, hasta que se hacen</p>
+			</div>
+
+
+			<div class="col text-center" >
+				<img  width="40%" src="imagenes/img1.jpeg" >
+			</div>
+		</div>
+		<br>
+
+		<div id="estudios" class="container-fluid" style="background-color:#E7D0FF;">
+			<div class="row text-center">
+				<h1 class="display-1" align="center" style="background-color:#E7D0FF;">Estudios</h1>
+
+
+				<div class="container">
+					<div class="row align-items-start">
+						<div class="col">
+
+
+							<table class="table ">
+								<thead>
+									<tr>
+										<th scope="col" style="background-color: #FCFCFC;">Institución educativa</th>
+
+									</tr>
+								</thead>
+								<tbody>
+									<tr align="left" class="text-center">
+										<td>Escuela Secundaria Tecnica No. 61</td>
+									</tr>
+
+									<tr align="left" class="text-center">
+										<td>Centro de estudios Tecnologicos, Industriales y de Servicios Cetis 17</td>
+									</tr>
+
+									<tr align="left" class="text-center">
+										<td colspan="2">Universidad Tecnologoica de Huejotzingo</td>
+									</tr>
+								</tbody>
+
+								<thead>
+									<tr>
+										<th scope="col" style="background-color: #FCFCFC";>Cursos</th>
+									</tr>
+								</thead>
+
+								<tbody>
+									<tr align="left" class="text-center">
+										<td>NDG Linux Unhatched course in the Cisco Networking Academy.</td>
+									</tr>
+
+									<tr align="left" class="text-center">
+										<td>Html</td>
+									</tr>
+
+									<tr align="left" class="text-center">
+										<td colspan="2">CSS</td>
+									</tr>
+
+
+									<tr align="left" class="text-center">
+										<td colspan="2">Scrum Fundamentals Certified</td>
+									</tr>
+								</tbody>
+
+								<thead>
+									<tr>
+										<th scope="col" style="background-color: #FCFCFC;">Conocimientos y Habilidades</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr align="left" class="text-center">
+										<td>Excel</td>
+									</tr>
+
+									<tr align="left" class="text-center">
+										<td>MySQL Workbench</td>
+									</tr>
+
+									<tr align="left" class="text-center">
+										<td colspan="2">Sublime Text</td>
+									</tr>
+								</tbody>
+							</table>
+
+						</div>
+
+						<div class="col">
+							<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" >
+								<div class="carousel-indicators">
+									<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+									<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+									<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+								</div>
+								<div class="carousel-inner">
+									<div class="carousel-item active">
+										<img src="imagenes/img2.jpg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="imagenes/img3.jpg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="imagenes/img4.jpg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+										</div>
+									</div>
+
+									<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+										<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+										<span class="visually-hidden">Previous</span>
+									</button>
+									<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+										<span class="carousel-control-next-icon" aria-hidden="true"></span>
+										<span class="visually-hidden">Next</span>
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<br>
+		<br>
+		<div id="portafolio" class="container" >
+			<div class="row text-center">
+				<h1 class="display-1" style="background-color:#E7D0FF;">Portafolio</h1>
+				<div class="row align-items-start">
+
+					<div class="col">
+						<div class="card mb-3">
+							<div class="row g-0">
+								<div class="col-md-4">
+									<img src="imagenes/img6.jpeg" class="img-fluid rounded-start" alt="...">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h3>Primer cuatrimestre</h3>
+										<p>Empresa dedica a la satisfacin del cliente siendo empaticos y colaborar correctamete en el ambito laboral</p>
+
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col">
+						<div class="card mb-3">
+							<div class="row g-0">
+								<div class="col-md-4">
+									<img src="imagenes/img6.jpeg" class="img-fluid rounded-start" alt="...">
+								</div>
+								<div class="col-md-8">
+									<div class="card-body">
+										<h3>Segundo cuatrimestre</h3>
+										<p class="card-text">Consta de una aplicacion para escuchar musica de una manera muy interactiva y gratuita,la musica debe de ser gratis</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<br>
+					<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<img src="imagenes/img7.jpeg" class="d-block w-100" alt="..." width="640" height="550">
+							</div>
+							<div class="carousel-item">
+								<img src="imagenes/img8.jpeg" class="d-block w-100" alt="..." width="640" height="550">
+							</div>
+							<div class="carousel-item">
+								<img src="imagenes/img9.jpeg" class="d-block w-100" alt="..." width="640" height="550">
+							</div>
+						</div>
+						<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="visually-hidden">Previous</span>
+						</button>
+						<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="visually-hidden">Next</span>
+						</button>
+					</div>
+				</div>
+			</div>
+			<br>
+			<div class="row" id="intereses" align="center">
+				<div class="col-12">
+					<h1 class="display-1" align="center" style="background-color:#E7D0FF;">Intereses</h1>
+<br>
+					<div class="row">
+						<div class="col-4">
+							<div class="card">
+								
+								<i class="fa-solid fa-futbol" style="font-size:48px"></i>
+								<div class="card-body">
+									<h3>Hacer deporte</h3>
+									<p>Me considero alguien que hace deporte por gusto, me hace sentir bien con mi cuerpo y conmigo misma</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-4">
+							<div class="card">
+								<i class="fa-solid fa-graduation-cap" style="font-size:48px"></i>
+								<h4>Estudiar </h4>
+								<div class="card-body">
+									<p>Me dedico actualmente solo a estudiar ya que tengo muchas metas por cumplir y planes a futuro</p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-4">
+							<div class="card">
+								<i class="fa-solid fa-people-roof" style="font-size:48px"></i>
+
+								<h3>Convivir en familia</h3>
+								<div class="card-body">
+									<p >Una de mis actividades favorita es pasar tiempo con mi familia y hablar entre nosotros</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<br>
+			<div id="contacto" class="container-fluid" style="background-color:#E7D0FF; color:black;">
+				<div class="row text-center">
+					<h1 class="display-1">Contacto</h1>
+
+				</div>
+				<br>
+				<div class="row">
+					<div class="col-md-6 text-center">
+
+						<h3>REDES SOCIALES</h3>
+						<H1>
+							<a href="https://www.facebook.com/devany.carmen/"><i class="fa-brands fa-facebook"></i></a> 
+							<i class="fa-brands fa-instagram"></i><br>	
+
+							<br>
+
+						</div>
+						<div class="col-md-6 text-center">
+
+							<span class="material-icons">location_on </span> San Martin Texmelucan Puebla <br>
+							<span class="material-icons">phone </span> 2484923936 <br>
+							<span class="material-icons">mail </span> 3521110584@uth.edu.mx 	
+						</div>
+					</div>
+				</div>
+				<br>
+				<p align="center">Derechos Reservados 2022</p>
+
+
+
+
+
+
+				<script src="js/bootstrap.bundle.min.js"></script>
+
+			</body>
+			</html>
